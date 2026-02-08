a37bb5b696cfd7681e2efed07aaacec45698cc2d
Luca <cappelletti.luca94@gmail.com>
2026-02-08 22:22:03 +0100

    Added SQLite hooks helper methods

--- diesel/src/sqlite/mod.rs
+++ diesel/src/sqlite/mod.rs
@@ -17,6 +17,7 @@ pub use self::connection::SerializedDatabase;
 pub use self::connection::SqliteBindValue;
 pub use self::connection::SqliteConnection;
 pub use self::connection::SqliteValue;
+pub use self::connection::{ChangeHookId, SqliteChangeEvent, SqliteChangeOp, SqliteChangeOps};
 pub use self::query_builder::SqliteQueryBuilder;
 
 /// Trait for the implementation of a SQLite aggregate function
